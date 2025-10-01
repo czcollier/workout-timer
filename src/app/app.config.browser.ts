ed53c5dfc61a0c3a1dd4741b5d73e531c3618284
czcollier <czcollier@gmail.com>
2025-10-01 16:38:51 +0000

    adding app.config.browser.ts in case it helps.
    Also adds env config in .idx

---
+++ src/app/app.config.browser.ts
@@ -0,0 +1,17 @@
+import { ApplicationConfig } from '@angular/core';
+import { appConfig } from './app.config';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getStorage, provideStorage } from '@angular/fire/storage';
+import { environment } from '../environments/environment';
+
+export const browserAppConfig: ApplicationConfig = {
+  providers: [
+    ...appConfig.providers,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideStorage(() => getStorage())
+  ]
+};
